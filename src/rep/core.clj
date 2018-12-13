5da1b4571bf793c831f41216bffa5d1d8c75a01e
Jason Felice <jason.m.felice@gmail.com>
2018-12-13 09:51:38 -0500

    Get nrepl port

--- src/rep/core.clj
+++ src/rep/core.clj
@@ -3,6 +3,11 @@
    [nrepl.core :as nrepl])
   (:gen-class))
 
+(defn- nrepl-port
+  []
+  (Long/parseLong (slurp ".nrepl-port")))
+
 (defn -main
   [& args]
+  (println "port: " (nrepl-port))
   (println "Hello, world!"))
