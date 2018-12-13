3fa4b2bbbe58d44d08699c3253d5cd08864ca7ee
Jason Felice <jason.m.felice@gmail.com>
2018-12-13 09:48:12 -0500

    Configure for Graal native-image

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [nrepl/nrepl "0.4.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]
+                 [nrepl/nrepl "0.5.1"]]
   :main ^:skip-aot rep.core
+  :native-image {:name "rep"
+                 :opts ["--verbose"]}
   :target-path "target/%s"
   :plugins [[io.taylorwood/lein-native-image "0.3.0"]]
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                       :native-image {:opts ["-Dclojure.compiler.direct-linking=true"]}}})
+                                      
--- src/rep/core.clj
+++ src/rep/core.clj
@@ -1,7 +1,8 @@
 (ns rep.core
+  (:require
+   [nrepl.core :as nrepl])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, world!"))
