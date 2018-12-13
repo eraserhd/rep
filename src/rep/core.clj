3ddf843a1e0450cf80070ef4be4b9d3fe5a7f914
Jason Felice <jason.m.felice@gmail.com>
2018-12-13 10:29:26 -0500

    Implement -h, --help

--- src/rep/core.clj
+++ src/rep/core.clj
@@ -1,7 +1,11 @@
 (ns rep.core
   (:require
+   [clojure.tools.cli :refer [parse-opts]]
    [nrepl.core :as nrepl])
   (:gen-class))
+
+(def ^:private cli-options
+  [["-h" "--help" "Show this help message."]])
 
 (defn- nrepl-port
   []
@@ -9,5 +13,26 @@
 
 (defn -main
   [& args]
-  (println "port: " (nrepl-port))
-  (println "Hello, world!"))
+  (let [{:keys [errors options arguments summary]} (parse-opts args cli-options)]
+    (cond
+     (seq errors)
+     (binding [*out* *err*]
+       (doseq [error errors]
+         (println error))
+       (System/exit 1))
+
+     (:help options)
+     (do
+       (println "rep: single-shot nREPL client")
+       (println "Syntax:")
+       (println "  rep [OPTIONS] CODE ...")
+       (println)
+       (println "Options:")
+       (println summary)
+       (println)
+       (System/exit 0))
+
+     :else
+     (do
+       (println "port: " (nrepl-port))
+       (println "Hello, world!")))))
