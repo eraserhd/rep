0525b63021145e5e77c8754a94cf34c72b0d9fc5
Jason Felice <jason.m.felice@gmail.com>
2018-12-13 15:51:22 -0500

    Make build with native-image again

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]
-                 [org.clojure/tools.cli "0.4.1"]
                  [nrepl/nrepl "0.5.1"]]
   :main ^:skip-aot rep.core
   :native-image {:name "rep"
--- src/rep/core.clj
+++ src/rep/core.clj
@@ -1,38 +1,22 @@
 (ns rep.core
   (:require
-   [clojure.tools.cli :refer [parse-opts]]
    [nrepl.core :as nrepl])
   (:gen-class))
-
-(def ^:private cli-options
-  [["-h" "--help" "Show this help message."]])
 
 (defn- nrepl-port
   []
   (Long/parseLong (slurp ".nrepl-port")))
 
+(defn- help
+  []
+  (println "rep: single-shot nREPL client")
+  (println "Syntax:")
+  (println "  rep [OPTIONS] CODE ...")
+  (println)
+  (println "Options:")
+  (println)
+  (System/exit 0))
+
 (defn -main
   [& args]
-  (let [{:keys [errors options arguments summary]} (parse-opts args cli-options)]
-    (cond
-     (seq errors)
-     (binding [*out* *err*]
-       (doseq [error errors]
-         (println error))
-       (System/exit 1))
-
-     (:help options)
-     (do
-       (println "rep: single-shot nREPL client")
-       (println "Syntax:")
-       (println "  rep [OPTIONS] CODE ...")
-       (println)
-       (println "Options:")
-       (println summary)
-       (println)
-       (System/exit 0))
-
-     :else
-     (do
-       (println "port: " (nrepl-port))
-       (println "Hello, world!")))))
+  (help))
