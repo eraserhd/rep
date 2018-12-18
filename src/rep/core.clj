bdc20fc5dced99fd31ede88c1a6b73f630fe2832
Jason Felice <jason.m.felice@gmail.com>
2018-12-18 09:23:48 -0500

    Solve timeout issue

--- src/rep/core.clj
+++ src/rep/core.clj
@@ -18,22 +18,52 @@
   (println)
   (System/exit 0))
 
+(defn- contains-status? [message status]
+  (contains? (into #{} (:status message)) status))
+
+(defn- take-until
+  "Transducer like take-while, except keeps the last value tested."
+  [pred]
+  (fn [rf]
+    (fn
+      ([] (rf))
+      ([result] (rf result))
+      ([result input]
+       (let [result (rf result input)]
+         (cond
+          (reduced? result) result
+          (pred input)      (reduced result)
+          :else             result))))))
+
+(defn- print-values
+  "Effectful transducer which prints REPL output."
+  [rf]
+  (fn
+    ([] (rf))
+    ([result] (rf result))
+    ([result input]
+     (when-some [value (:value input)]
+       (println value))
+     (rf result input))))
+
+(defn- null-reducer
+  "A reducing function which does nothing."
+  ([] nil)
+  ([result] result)
+  ([result input] result))
+
 (defn rep
   [& args]
   (let [conn (nrepl/connect :port (nrepl-port))
         client (nrepl/client conn 60000)
         session (nrepl/client-session client)
         msg-seq (session {:op "eval" :code (apply str args)})]
-    (loop [[{:keys [status value out err]} & msgs] msg-seq]
-      (when out
-        (print out))
-      (when err
-        (binding [*out* *err*]
-          (print err)))
-      (when value
-        (println value))
-      (when-not (contains? (into #{} status) "done")
-        (recur msgs)))
+    (transduce
+      (comp
+        (take-until #(contains-status? % "done"))
+        print-values)
+      null-reducer
+      msg-seq)
     (let [^java.io.Closeable cc conn]
       (.close cc))
     0))
