eca41ce95564ce94530999afd9ee464a662ecec1
Jason Felice <jason.m.felice@gmail.com>
2019-01-04 11:35:29 -0500

    Use maven a different way

--- deps/build.sh
+++ deps/build.sh
@@ -25,6 +25,15 @@ install_graal() {
 install_graal macos
 install_graal linux
 
+if  [ -x maven/bin/mvn ]; then
+    cd deps
+    rm -rf maven/
+    curl -Lo maven.tar.gz http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+    tar xzf maven.tar.gz
+    mv apache-maven-* maven/
+    cd
+fi
+
 source deps/path.sh
 
 # Build a patched Clojure (see CLJ-1472)
--- deps/path.sh
+++ deps/path.sh
@@ -1,6 +1,6 @@
 if [ `uname` = Darwin ]; then
-    PATH="$PWD/deps/graalvm-macos/Contents/Home/bin":$PATH
+    PATH="$PWD/deps/graalvm-macos/Contents/Home/bin":"$PWD/maven/bin":$PATH
 else
-    PATH="$PWD/graalvm-linux/bin":$PATH
+    PATH="$PWD/graalvm-linux/bin":"$PWD/maven/bin":$PATH
 fi
 export PATH
