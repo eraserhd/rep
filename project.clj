ed67f08b0a15e0b201eac858d00d5c8d2aaab5af
Jason Felice <jason.m.felice@gmail.com>
2019-11-05 21:40:23 -0500

    Bump required leiningen version to 2.9.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/eraserhd/rep"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.8.3"
+  :min-lein-version "2.9.1"
   :dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]
                  [org.clojure/spec.alpha "0.2.177-SNAPSHOT"]
                  [org.clojure/tools.cli "0.4.1"]
@@ -20,7 +20,7 @@
   :target-path "target/%s"
   :plugins [[io.taylorwood/lein-native-image "0.3.0"]
             [lein-midje "3.2.1"]]
-  :profiles {:dev {:dependencies [[midje "1.9.4"]]}}
+  :profiles {:dev {:dependencies [[midje "1.9.4"]]}
              :repl {:dependencies [[nrepl/nrepl "0.5.3"]]}
              :uberjar {:aot :all
-                       :native-image {:opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :native-image {:opts ["-Dclojure.compiler.direct-linking=true"]}}})
