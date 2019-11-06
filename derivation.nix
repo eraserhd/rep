8936e1325661be09576f0ac2cea78da883c094e9
Jason Felice <jason.m.felice@gmail.com>
2019-11-06 10:50:32 -0500

    It builds in Nix

--- derivation.nix
+++ derivation.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs, fetchurl, graalvm8, leiningen, maven, jdk, ... }:
+{ stdenv, pkgs, clojure, fetchurl, graalvm8, leiningen, maven, jdk, ... }:
 
 let
   java-deps = import ./deps.nix { inherit pkgs; };
@@ -9,15 +9,33 @@ in stdenv.mkDerivation {
   src = ./.;
 
   buildInputs = [
+    clojure
     graalvm8
     leiningen
     maven
   ] ++ map (x: x.path) java-deps.packages;
 
   buildPhase = ''
-    mkdir -p home
-    export HOME=$(pwd)/home
-    lein native-image
+    rm -rf classes/
+    mkdir classes/
+    for jar in deps/*; do
+      CLASSPATH="$jar:$CLASSPATH"
+    done
+    export CLASSPATH
+    java -cp src:$CLASSPATH clojure.main -e "(compile 'rep.core)"
+    native-image \
+      -cp classes:$CLASSPATH \
+      --verbose \
+      --no-server \
+      -J-Xmx3g \
+      --report-unsupported-elements-at-runtime \
+      -H:ReflectionConfigurationFiles=reflectconfig.json \
+      --initialize-at-build-time \
+      rep.core
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp rep.core $out/bin/rep
   '';
 
   meta = with stdenv.lib; {
