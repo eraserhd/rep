1951df780fdd2781644f934dfc36ee394460effb
Jason Felice <jason.m.felice@gmail.com>
2019-11-07 00:50:49 -0500

    Fix kakoune plugin directory

--- derivation.nix
+++ derivation.nix
@@ -35,10 +35,10 @@ in stdenv.mkDerivation {
     a2x -f manpage rep.1.adoc
   '';
   installPhase = ''
-    mkdir -p $out/bin/ $out/share/man/man1/ $out/share/kak/plugins/
+    mkdir -p $out/bin/ $out/share/man/man1/ $out/share/kak/autoload/plugins/
     cp rep $out/bin/
     cp rep.1 $out/share/man/man1/
-    cp rc/rep.kak $out/share/kak/plugins/
+    cp rc/rep.kak $out/share/kak/autoload/plugins/
   '';
 
   meta = with stdenv.lib; {
