2f18fff97a7f2805688312b4e3909a6926d4c011
Jason Felice <jason.m.felice@gmail.com>
2019-11-06 23:19:55 -0500

    Run unit tests

--- release.nix
+++ release.nix
@@ -3,14 +3,17 @@ let
   pkgs = import nixpkgs { config = {}; };
   rep = pkgs.callPackage ./derivation.nix {};
 in {
-  unit-test = pkgs.stdenv.mkDerivation {
+  unit-test = let
+    java-deps = import ./test-deps.nix { inherit pkgs; };
+  in pkgs.stdenv.mkDerivation {
     name = "rep-unit-tests";
     src = ./.;
-    buildInputs = with pkgs; [ rep jdk ];
+    buildInputs = with pkgs; [ rep jdk ] ++ map (x: x.path) java-deps.packages;
     buildPhase = ''
       for jar in deps/*; do
         CLASSPATH="$jar:$CLASSPATH"
       done
+      CLASSPATH=test:src:$CLASSPATH
       export CLASSPATH
       REP_TEST_DRIVER=native \
         REP_TO_TEST='${rep}/bin/rep' \
