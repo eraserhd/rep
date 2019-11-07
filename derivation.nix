2ad71cc52ecc68da699133b474954f4fb7571dff
Jason Felice <jason.m.felice@gmail.com>
2019-11-07 00:20:49 -0500

    Install rep.kak

--- derivation.nix
+++ derivation.nix
@@ -35,9 +35,10 @@ in stdenv.mkDerivation {
     a2x -f manpage rep.1.adoc
   '';
   installPhase = ''
-    mkdir -p $out/bin $out/share/man/man1
-    cp rep $out/bin/rep
+    mkdir -p $out/bin/ $out/share/man/man1/ $out/share/kak/plugins/
+    cp rep $out/bin/
     cp rep.1 $out/share/man/man1/
+    cp rc/rep.kak $out/share/kak/plugins/
   '';
 
   meta = with stdenv.lib; {
